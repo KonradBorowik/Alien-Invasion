f2484d2afefb2129ad7fd766b86daa9963976618
Konrad <konrad1borowik@gmail.com>
2022-02-23 13:24:17 +0100

    changed backgorund color

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,20 +11,24 @@ class AlienInvasion:
 		self.screen = pygame.display.set_mode((1200,800))
 		pygame.display.set_caption("Alein Invasion")
 
+		# set the background color
+		self.bg_color = (29, 147, 209)
+
 	def run_game(self):
 		"""Start the main loop for the game"""
 		while True:
-			#Watch for keyboard and mouse events"""
+			# Watch for keyboard and mouse events"""
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
 					sys.exit()
+			# Redraw the screen during each pass through the loop
+			self.screen.fill(self.bg_color)
 
-		"""Make the most recently drawn screen visible"""
-		pygame.display.flip()
+			"""Make the most recently drawn screen visible"""
+			pygame.display.flip()
 
 
 if __name__ == "__main__":
 	# Make a game instance and runt the game
 	ai = AlienInvasion()
 	ai.run_game()
-	
