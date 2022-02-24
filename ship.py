1fda34f7234cd208bc180ecf6888e1b7387f16b0
Konrad <konrad1borowik@gmail.com>
2022-02-24 10:53:31 +0100

    ship moves up and down

--- ship.py
+++ ship.py
@@ -18,21 +18,30 @@ class Ship:
 
 		# store a decimal value for the ship's horizontal position
 		self.x = float(self.rect.x)
+		# store a decimal value for the ship's vertical position
+		self.y = float(self.rect.y)
 
 		# movement flag
 		self.moving_right = False
 		self.moving_left = False
+		self.moving_up = False
+		self.moving_down = False
 
 	def update(self):
 		"""Update the ship's position based on the movement flags"""
-		# update the ship's x value, not the rect
+		# update the ship's x or y value, not the rect
 		if self.moving_right and self.rect.right < self.screen_rect.right:
 			self.x += self.settings.ship_speed
-		if self.moving_left and self.rect.left > 0:
+		elif self.moving_left and self.rect.left > 0:
 			self.x -= self.settings.ship_speed
+		elif self.moving_up and self.rect.top > 0:
+			self.y -= self.settings.ship_speed
+		elif self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+			self.y += self.settings.ship_speed
 
-		# update rect object from self.x
+		# update rect object from self.x and self.y
 		self.rect.x = self.x
+		self.rect.y = self.y
 
 	def blitme(self):
 		"""Draw the ship at its current location"""
